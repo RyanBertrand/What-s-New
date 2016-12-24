66f6f9b80968afcd4353decb9843749f4d0f5205
Ryan Bertrand <rabertrand21@gmail.com>
2016-12-23 18:21:18 -0800

    call didDismissCompletionHandler when dismissing

--- Classes/MTZWhatsNewViewController/MTZWhatsNewViewController.m
+++ Classes/MTZWhatsNewViewController/MTZWhatsNewViewController.m
@@ -132,6 +132,9 @@
 - (IBAction)didTapContinueButton:(id)sender
 {
 	[self dismissViewControllerAnimated:YES completion:nil];
+	if(self.didDismissCompletionHandler){
+            self.didDismissCompletionHandler();
+        }
 }
 
 
